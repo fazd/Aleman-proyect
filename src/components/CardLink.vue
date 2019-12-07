b5e30e0b6a74ce152ebf5a0abbe0018321ceac72
Fazd <fabioandres2907@hotmail.com>
2019-12-07 01:16:43 -0500

    home improved

---
+++ src/components/CardLink.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-hover>
+    <template v-slot:default="{ hover }">
+        <v-card>
+        <v-img class="align-end" height="250px" width="250px" :src="src" >
+            <v-card-title>{{title}}</v-card-title>
+        </v-img>    
+        <v-fade-transition>
+          <v-overlay
+            v-if="hover"
+            absolute
+            color="#036358"
+          >
+            <v-btn>See more info</v-btn>
+          </v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
+
+</template>
+
+<script>
+export default {
+    props:{
+        title: String,
+        src: String
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,66 @@
 <template>
   <v-content>
     <v-container fluid class="fill-height">
-      <v-row>
+      <v-row class="mb-5">
         <v-col>
-            <!-- <h1>Galeria</h1> -->
-            <v-carousel
-              cycle
-              hide-delimiter-background
-              show-arrows-on-hover
-            >
-              <v-carousel-item
-                v-for="(slide, i) in slides"
-                :key="i"
-              >
-                <v-sheet
-                  :color="colors[i]"
-                  height="100%"
-                >
-                  <v-row
-                    class="fill-height"
-                    align="center"
-                    justify="center"
-                  >
-                    <div class="display-3">{{ slide }} Slide</div>
-                  </v-row>
-                </v-sheet>
-              </v-carousel-item>
-            </v-carousel>
+          <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
+            <v-carousel-item v-for="(slide, i) in slides" :key="i">
+              <v-sheet :color="colors[i]" height="100%">
+                <v-row class="fill-height" align="center" justify="center">
+                  <div class="display-3">{{ slide }} Slide</div>
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-row>
+      <h2>Links para practicar</h2>
+      <v-row class="mb-5">
+        <v-col col="12" sm="8" md="3">
+          <CardLink title="Hören" src="http://german-akademie.com/wp-content/uploads/2017/05/deutsch-lernen-durch-horen-hor-mal-12-meine-handtasche-ist-weg.jpg"></CardLink>
+        </v-col>
+        <v-col col="12" sm="8" md="3">
+          <CardLink title="Hören" src="http://german-akademie.com/wp-content/uploads/2017/05/deutsch-lernen-durch-horen-hor-mal-12-meine-handtasche-ist-weg.jpg"></CardLink>
+
+        </v-col>
+        <v-col col="12" sm="8" md="3">
+          <CardLink title="Hören" src="http://german-akademie.com/wp-content/uploads/2017/05/deutsch-lernen-durch-horen-hor-mal-12-meine-handtasche-ist-weg.jpg"></CardLink>
+        </v-col>
+        <v-col col="12" sm="8" md="3">
+          <CardLink title="Hören" src="http://german-akademie.com/wp-content/uploads/2017/05/deutsch-lernen-durch-horen-hor-mal-12-meine-handtasche-ist-weg.jpg"></CardLink>
+        </v-col>
+        
+      </v-row>
+      <h2>Sobre nosotros</h2>
+      <v-row  align="center" justify="center" v-for="(user,i) in users.length/2" :key="i" >
+        <v-col cols="12" sm="8" md="5">
+          <v-card color="dark" dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="headline">{{users[2*i].name}}</v-card-title>
+                <v-card-subtitle>{{users[2*i].text}}</v-card-subtitle>
+              </div>
+
+              <v-avatar class="ma-5" size="125" fab>
+                <v-img :src="users[2*i].src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="8" md="5">
+          <v-card color="dark" dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="headline">{{users[2*i+1].name}}</v-card-title>
+                <v-card-subtitle>{{users[2*i+1].text}}</v-card-subtitle>
+              </div>
+
+              <v-avatar class="ma-5" size="125" fab>
+                <v-img :src="users[2*i+1].src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+          
         </v-col>
       </v-row>
     </v-container>
@@ -35,32 +69,124 @@
 
 <script>
 // @ is an alias to /src
-
+import CardLink from '../components/CardLink.vue'
 export default {
-  name: 'home',
-  components: {
-
-  },
-  data () {
+  name: "home",
+  components: {CardLink},
+  data() {
     return {
       colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4',
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4"
       ],
-      slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth',
-      ],
-    }
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+      users: [
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+        {
+          name: "Fabio Andres",
+          text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nesciunt dolor odit dolorem consequuntur nemo tenetur quam ipsam ducimus, libero tempore sit enim accusamus, numquam sint fugit corporis quos voluptatibus.",
+          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+        },
+      ]
+    };
   },
-  methods: {
-    
-  }
-}
+  methods: {}
+};
 </script>
